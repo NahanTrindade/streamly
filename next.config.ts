3a749cec70cc95a7dea06bfb21028a7250180800
NahanTrindade <nahantrindade.p@gmail.com>
2025-02-10 19:06:45 -0300

    build:

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: { remotePatterns: [{ hostname: "utfs.io" }] },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      crypto: false,
-    };
+  webpack: (config, { isServer, webpack }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: require.resolve("stream-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+      };
+
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          process: "process/browser",
+        }),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        new webpack.NormalModuleReplacementPlugin(/node:crypto/, (resource: any) => {
+          resource.request = resource.request.replace(/^node:/, "");
+        })
+      );
+    }
     return config;
   },
   /* config options here */
